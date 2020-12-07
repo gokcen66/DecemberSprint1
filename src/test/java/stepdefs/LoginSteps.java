eb7619fb4a638df3ccf5ef953314ba52bafa4c18
atezgider <atezgider@gmail.com>
2020-12-08 00:33:17 +0100

    ahmet 2020-12-06

--- src/test/java/stepdefs/LoginSteps.java
+++ src/test/java/stepdefs/LoginSteps.java
@@ -4,6 +4,9 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.testng.Assert;
+import pageModels.NavMenu;
+import utilities.Buttons;
+import utilities.NotificationResults;
 import utilities.ParentClass;
 
 import static utilities.ElementsClass.$$;
@@ -23,5 +26,31 @@ public class LoginSteps extends ParentClass {
     @Then("^login should be successfull$")
     public void loginShouldBeSuccessfull() {
         Assert.assertTrue($$(DashBoard).getElements().size()>0);
+    }
+
+    @Then("^the new input should be created$")
+    public void theNewInputShouldBeCreated() {
+        verifyNotificationAs(NotificationResults.created);
+    }
+
+    @Then("^the input should be updated$")
+    public void theInputShouldBeUpdated() {
+        verifyNotificationAs(NotificationResults.updated);
+    }
+
+    @Then("^the input should be deleted$")
+    public void theInputShouldBeDeleted() {
+        verifyNotificationAs(NotificationResults.deleted);
+    }
+
+    @Then("^the new input should not be created$")
+    public void theNewInputShouldNotBeCreated() {
+        clickToDialogButton(Buttons.Close);
+        verifyNotificationAs(NotificationResults.Error);
+    }
+
+    @When("^user click to Dashboard$")
+    public void userClickToDashboard() {
+        clickOnMenuTo(NavMenu.Dashboard);
     }
 }
